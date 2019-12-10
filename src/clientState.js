e6150cc8bc52a968cbd26db4be2a2a2f8d242d2e
sunjae <azuretic94@gmail.com>
2019-12-10 23:45:54 +0900

    아폴로 설정

--- src/clientState.js
+++ src/clientState.js
@@ -1,40 +1,29 @@
 export const defaults = {
-    notes: [
-      {
-        __typename: "Note",
-        id: 1,
-        title: "First",
-        content: "Second"
-      }
-    ]
-  };
-  export const typeDefs = [
-    `
-      schema {
-          query: Query
-          mutation: Mutation
-      }
-      type Query {
-          notes: [Note]!
-          note(id: Int!): Note
-      }
-      type Mutation{
-          createNote(title: String!, content: String!): Note
-          editNote(id: String!, title: String!, content:String!): Note
-      }
-      type Note{
-          id: Int!
-          title: String!
-          content: String!
-      }
-      `
-  ];
-  export const resolvers = {
-    Query: {
-      note: (_, variables, { getCacheKey }) => {
-        const id = getCacheKey({ __typename: "Note", id: variables.id });
-        console.log(id);
-        return null;
-      }
+  notes: []
+};
+export const typeDefs = [
+  `
+    schema {
+        query: Query
+        mutation: Mutation
     }
-  };
+    type Query {
+        notes: [Note]!
+        note(id: Int!): Note
+    }
+    type Mutation{
+        createNote(title: String!, content: String!)
+        editNote(id: String!, title: String!, content:String!)
+    }
+    type Note{
+        id: Int!
+        title: String!
+        content: String!
+    }
+    `
+];
+export const resolvers = {
+  Query: {
+    notes: () => true
+  }
+};
